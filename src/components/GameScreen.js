1dc6d85dcfced76966c64d8d57870d895a54c02c
mediacloner <alexbcn@DESKTOP-6QOCE5T.localdomain>
2018-02-18 19:31:05 +0100

    add button extras

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Jumbotron, Container, Row, Col, Button } from "reactstrap";
+import { Jumbotron, Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Input } from "reactstrap";
 import people from "./../img/people.svg";
 import planetHearth from "./../img/planetHearth.svg";
 import imgPlayer1 from "./../img/player1.svg";
@@ -20,14 +20,20 @@ mapboxgl.accessToken =
 let mapDesign = "mapbox://styles/mediacloner/cjds044272vwt2sp91rj2po6n";
 
 class GameScreen extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       lng: -2,
       lat: 54,
-      zoom: 6
-    };
+      zoom: 6,
+      modal: false
+    }
+
+    this.toggle = this.toggle.bind(this);
   }
+
+  toggle() {this.setState({ modal: !this.state.modal  });}
+
 
   updateMap() {
     if (
@@ -102,16 +108,12 @@ class GameScreen extends Component {
   render() {
     const { lng, lat, zoom } = this.state;
 
-    return (
-      <Container>
+    return <Container>
         <div>
           <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
             {/* <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div> */}
           </div>
-          <div
-            ref={el => (this.mapContainer = el)}
-            className="absolute top right left bottom"
-          />
+          <div ref={el => (this.mapContainer = el)} className="absolute top right left bottom" />
         </div>
 
         <Jumbotron>
@@ -122,100 +124,84 @@ class GameScreen extends Component {
             <Container>
               <Row>
                 <Col>
-                  {this.props.focusPlayer === 1 ? (
-                    <img
-                      src={imgPlayer1}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"
-                    />
-                  ) : (
-                    <img
-                      src={imgPlayer1Des}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"
-                    />
-                  )}
+                  {this.props.focusPlayer === 1 ? <img src={imgPlayer1} className="img-fluid justify-content-center" alt="Responsive" /> : <img src={imgPlayer1Des} className="img-fluid justify-content-center" alt="Responsive" />}
                   <div className="score1">{this.props.score1}</div>
                   <div className="player">{this.props.player1}</div>
                 </Col>
                 <Col>
                   <div>
-
-
-
-                    { this.props.arrowsUI === 'more' ?
-
-                      <img src={more}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"/> 
-                      
-                      : this.props.arrowsUI === 'less' ? <img src={less}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"/> 
-                      
-                      
-                      :  this.props.arrowsUI === 'right' ? <img src={right}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"/> : <img src={noActive}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"/> 
-                        
-                       
-                    }
-                    
+                    {this.props.arrowsUI === "more" ? <img src={more} className="img-fluid justify-content-center" alt="Responsive" /> : this.props.arrowsUI === "less" ? <img src={less} className="img-fluid justify-content-center" alt="Responsive" /> : this.props.arrowsUI === "right" ? <img src={right} className="img-fluid justify-content-center" alt="Responsive" /> : <img src={noActive} className="img-fluid justify-content-center" alt="Responsive" />}
 
                     <h5>{this.props.messages}</h5>
                   </div>
                 </Col>
                 <Col>
-                  {this.props.focusPlayer === 2 ? (
-                    <img
-                      src={imgPlayer2}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"
-                    />
-                  ) : (
-                    <img
-                      src={imgPlayer2Des}
-                      className="img-fluid justify-content-center"
-                      alt="Responsive"
-                    />
-                  )}
+                  {this.props.focusPlayer === 2 ? <img src={imgPlayer2} className="img-fluid justify-content-center" alt="Responsive" /> : <img src={imgPlayer2Des} className="img-fluid justify-content-center" alt="Responsive" />}
                   <div className="score2">{this.props.score2}</div>
                   <div className="player">{this.props.player2}</div>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <input
-                    type="text"
-                    name="tries"
-                    placeholder="Try. 0.0 Millions Format "
-                    autoFocus={true}
-                    value={this.state.input}
-                    onChange={this.handleChange}
-                    required
-                  />&nbsp;{this.props.nextCountry !== 0 ? (
-                    <Button onClick={this.props.actionButton}>Next</Button>
-                  ) : (
-                    undefined
-                  )}
+                  <input type="text" name="tries" placeholder="Try. 0.0 Millions Format " autoFocus={true} value={this.state.input} onChange={this.handleChange} required />&nbsp;{this.props.nextCountry !== 0 ? <div>
+                      {" "}
+                      <Button onClick={this.props.actionButton}>
+                        Next
+                      </Button>
+                      <Button color="danger" onClick={this.toggle}>
+                        Extra Bonnus
+                      </Button>
+                      <Modal isOpen={this.state.modal} modalTransition={{ timeout: 20 }} backdropTransition={{ timeout: 10 }} toggle={this.toggle}>
+                        <ModalHeader toggle={this.toggle} className="bg-danger text-white">
+                          Extra Bonus
+                        </ModalHeader>
+                        <ModalBody className="bg-dark text-white">
+                          Try to guess capital and region of {}
+                          <FormGroup>
+                            <Label for="exampleSelect">Capital</Label>
+                            <Input type="select" name="select" id="exampleSelect">
+                              <option>Capital 1</option>
+                              <option>Capital 2</option>
+                              <option>Capital 3</option>
+                              <option>Capital 4</option>
+                              <option>Capital 5</option>
+                            </Input>
+                          </FormGroup>
+                          <FormGroup>
+                            <Label for="exampleSelect">Region</Label>
+                            <Input type="select" name="select" id="exampleSelect">
+                              <option>Region 1</option>
+                              <option>Region 2</option>
+                              <option>Region 3</option>
+                              <option>Region 4</option>
+                              <option>Region 5</option>
+                            </Input>
+                          </FormGroup>
+                        </ModalBody>
+                        <ModalFooter className="bg-secondary text-white">
+                          <Button color="primary" onClick={this.toggle}>
+                            Do Something
+                          </Button> <Button color="secondary" onClick={this.toggle}>
+                            Cancel
+                          </Button>
+                        </ModalFooter>
+                      </Modal>
+                    </div> : undefined}
                   <div className="card-header">
                     <h1>{this.props.name}</h1>
-                    <img
-                      src={this.props.flag}
-                      className="img-fluid justify-content-center flag"
-                      alt="Responsive"
-                    />
+                    <img src={this.props.flag} className="img-fluid justify-content-center flag" alt="Responsive" />
                   </div>
                 </Col>
               </Row>
             </Container>
           </form>
         </Jumbotron>
-      </Container>
-    );
+      </Container>;
   }
 }
+
+
+
+
 
 export default GameScreen;
